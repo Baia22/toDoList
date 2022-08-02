c893059e68fb985b95d16b2bde4e7917c0b78eaa
baia22 <baiatser2@gmail.com>
2022-08-02 20:29:48 +0400

    To-do

---
+++ main.js
@@ -0,0 +1,21 @@
+let input=document.querySelector(".input")
+let button=document.querySelector(".button")
+let tab=document.querySelector(".tab")
+
+button.addEventListener("click", function() {
+    let addMore=document.createElement("div")
+    addMore.classList.add("addMore")
+    tab.appendChild(addMore)
+    let par=document.createElement("p")
+    addMore.appendChild(par)
+    par.innerHTML=input.value
+    par.classList.add("par")
+    let button1=document.createElement("button")
+    button1.innerHTML="Delete"
+    addMore.appendChild(button1)
+
+    button1.addEventListener("click", function(){
+        addMore.style.display="none"
+    })
+})
+
